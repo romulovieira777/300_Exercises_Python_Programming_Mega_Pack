7d2c1ecb831916cde7331f9c209c2702080b76bf
Romulo Vieira <romulo.vieira777@gmail.com>
2023-07-28 09:44:53 -0300

    Print() Function

---
+++ Section_04_Print_Function/05_Exercise.py
@@ -0,0 +1,13 @@
+"""
+Exercise No. 05
+
+Assign 199.99 to the price variable and check the solution.
+
+Expected result:
+
+    This costs 199.99
+"""
+price = 199.99
+
+print("This costs", price)
+print(f"This costs {price}")
