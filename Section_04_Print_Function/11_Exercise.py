aec023bf7c4f63f6926e6c6ce9d1ec4c20a1668a
Romulo Vieira <romulo.vieira777@gmail.com>
2023-07-31 10:05:51 -0300

    Print() Function

---
+++ Section_04_Print_Function/11_Exercise.py
@@ -0,0 +1,8 @@
+"""
+Exercise No. 11
+
+Using the print() function and the sep argument set to '#' print the following text:
+
+    'summer#time#holiday'
+"""
+print('summer', 'time', 'holiday', sep='#')
