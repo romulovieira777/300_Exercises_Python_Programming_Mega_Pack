d0be4c9c34163c96b4a8c0c9ef572f447447a47a
Romulo Vieira <romulo.vieira777@gmail.com>
2023-08-15 09:23:51 -0300

    Data Types

---
+++ Section_07_Data_Types/30_Exercise.py
@@ -0,0 +1,16 @@
+"""
+Exercise No. 30
+
+Check if the following variable:
+
+    flag = False
+
+is an instance of the bool class and print the result to the console.
+
+Expected result:
+
+    True
+"""
+flag = False
+
+print(isinstance(flag, bool))
