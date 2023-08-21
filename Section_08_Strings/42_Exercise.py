248e3d20f5ace26d308a4ef97caad58a7ac7b2d9
Romulo Vieira <romulo.vieira777@gmail.com>
2023-08-21 09:33:41 -0300

    Strings

---
+++ Section_08_Strings/42_Exercise.py
@@ -0,0 +1,16 @@
+"""
+Exercise No. 42
+
+The following text is given:
+
+    text = 'Open,High,Low,Close'
+
+Using the appropriate method split the text by comma. Print the result to the console as shown below.
+
+Expected result:
+
+    ['Open', 'High', 'Low', 'Close']
+"""
+text = 'Open,High,Low,Close'
+
+print(text.split(','))
