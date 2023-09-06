d6434419fa4541e1b51e08bfa7fef3411711b8db
Romulo Vieira <romulo.vieira777@gmail.com>
2023-09-06 09:57:16 -0300

    If Statement

---
+++ Section_15_If_Statement/80_Exercise.py
@@ -0,0 +1,19 @@
+"""
+Exercise No. 80
+
+The following code is given:
+
+    code = 'DSVNDOICSN'
+
+Check if the code variable has only uppercase letters. If so, print 'YES' to the console.
+
+Expected result:
+
+    YES
+"""
+code = 'DSVNDOICSN'
+
+if code.isupper():
+    print('YES')
+else:
+    print('NO')
