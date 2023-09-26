507503e95f82507eba740d21c95bc98d3902ba21
Romulo Vieira <romulo.vieira777@gmail.com>
2023-09-26 09:18:15 -0300

    Built-in Functions

---
+++ Section_25_Builtin_Functions/119_Exercise.py
@@ -0,0 +1,24 @@
+"""
+Exercise No. 119
+
+The variable x:
+
+    x = -1.5
+
+and the following expression are given:
+
+    expression = 'x ** 2 + x'
+
+Using the appropriate function, calculate the value of this expression and print the result to the console.
+
+Tip: Use the eval() function.
+
+Expected result:
+
+    0.75
+"""
+x = -1.5
+expression = 'x ** 2 + x'
+
+calculation = eval(expression)
+print(calculation)
---
+++ Section_25_Builtin_Functions/121_Exercise.py
@@ -0,0 +1,21 @@
+"""
+Exercise No. 121
+
+The following list is given:
+
+    characters = ['k', 'b', 'c', 'j', 'z', 'w']
+
+Using the built-in functions, return the first and last letter in alphabetical order from this list and print the result
+to the console as shown below.
+
+Tip: Use the min() and max() functions.
+
+Expected result:
+
+    First: b
+    Last: z
+"""
+characters = ['k', 'b', 'c', 'j', 'z', 'w']
+
+print(f'First: {min(characters)}')
+print(f'Last: {max(characters)}')
